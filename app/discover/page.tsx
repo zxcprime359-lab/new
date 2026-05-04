ac19ff8a9d055f048f9551a30c369f9e4aac9921
jerometecson-main <jerometecson.main@gmail.com>
2026-05-04 20:07:53 +0800

    xxx

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 import ExploreTmdb from "@/app/explore";
-import { useParams } from "next/navigation";
 
 export default function DiscoverMovies() {
-  const params = useParams();
-  const parameter = String(params.parameter);
   return <ExploreTmdb media_type="movie" />;
 }
